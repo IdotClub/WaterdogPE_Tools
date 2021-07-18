9d3f9f158357bc4b616b3977d01645e5578b962c
Blackjack200 <lx826444126@gmail.com>
2021-07-18 20:31:53 +0800

    fix

--- src/blackjack200/wdpe/UpdateLatencyTask.php
+++ src/blackjack200/wdpe/UpdateLatencyTask.php
@@ -13,6 +13,7 @@ class UpdateLatencyTask extends Task {
 	public function onRun(int $currentTick) {
 		$pk = new ScriptCustomEventPacket();
 		$pk->eventName = 'waterdog:latency';
+		$pk->eventData = '';
 		foreach (Server::getInstance()->getOnlinePlayers() as $player) {
 			assert($player instanceof WaterdogPlayer);
 			$player->dataPacket($pk);
